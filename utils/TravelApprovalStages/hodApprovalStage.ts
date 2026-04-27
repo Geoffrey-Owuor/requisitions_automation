1bd92b6dba7ce1088b7eae67252d041fe6ba7bcb
Geoffrey-Owuor <geoffreyowuor22@gmail.com>
2026-04-27 12:54:24 +0300

    Finishing up on the email sending logic for various travel approval stages

--- utils/TravelApprovalStages/hodApprovalStage.ts
+++ utils/TravelApprovalStages/hodApprovalStage.ts
@@ -1,3 +1,6 @@
+import { EmailSender } from "@/services/EmailSender";
+import { HR_ARRAY } from "@/public/secretAssets";
+
 type HodApprovalStageProps = {
   uuid: string;
   userEmail: string;
@@ -13,4 +16,82 @@ export function hodApprovalStage({
   approverEmail,
   approverName,
   approvalTier,
-}: HodApprovalStageProps) {}
+}: HodApprovalStageProps) {
+  // HOD declined the request - Notify the HOD and submitter
+  if (status === "declined") {
+    // Hod
+    EmailSender({
+      to: approverEmail,
+      requestId: uuid,
+      message: "You have declined this travel requisition.",
+      title: "Final Update: Travel Requisition Declined",
+      role: "user",
+    });
+
+    // Submitter
+    EmailSender({
+      to: userEmail,
+      requestId: uuid,
+      message:
+        "Your travel requisition has been declined in the HOD approval stage",
+      title: `Final Update: Travel Requisition Declined By ${approverName}`,
+      role: "user",
+    });
+  }
+
+  // HOD approved request - check if tier 1 or 2 and send the appropiate emails
+  if (status === "approved") {
+    if (approvalTier === "Tier 1") {
+      // Hod
+      EmailSender({
+        to: approverEmail,
+        requestId: uuid,
+        message: "You have approved this travel requisition",
+        title: "Final Update: Travel Requisition Approved",
+        role: "user",
+        showPdfDownload: true,
+      });
+
+      // User
+      EmailSender({
+        to: userEmail,
+        requestId: uuid,
+        message: `Your travel requisition has been approved by ${approverName}`,
+        title: `Final Update: Travel Requisition Approved By ${approverName}`,
+        role: "user",
+        showPdfDownload: true,
+      });
+    } else {
+      //  Requisition requires the next approval - HR
+      HR_ARRAY.forEach((hrApprover) => {
+        EmailSender({
+          to: hrApprover.email,
+          requestId: uuid,
+          message:
+            "A new travel requisition has been submitted and requires your approval",
+          title: "Action Required: New Travel Requisition",
+          role: "HR",
+          reviewLink: `?token=${hrApprover.uuid}&stage=hr`,
+        });
+      });
+
+      // Notify involved parties (Hod and Submitter)
+      EmailSender({
+        to: approverEmail,
+        requestId: uuid,
+        message:
+          "You have approved this travel requisition. It has been forwarded to HR for the next approval stage",
+        title: "Update: Travel Requisition Approved",
+        role: "user",
+      });
+
+      EmailSender({
+        to: userEmail,
+        requestId: uuid,
+        message: `Your travel requisition has been approved by ${approverName} and has been forwaded to HR for the next approval stage`,
+        title: `Update: Travel Requisition Approved By ${approverName}`,
+        role: "user",
+      });
+    }
+  }
+}
--- utils/TravelApprovalStages/hrApprovalStage.ts
+++ utils/TravelApprovalStages/hrApprovalStage.ts
@@ -1,3 +1,6 @@
+import { DIRECTOR_ARRAY } from "@/public/secretAssets";
+import { EmailSender } from "@/services/EmailSender";
+
 type HrApprovalStageProps = {
   uuid: string;
   userEmail: string;
@@ -15,4 +18,142 @@ export function hrApprovalStage({
   approverEmail,
   approverName,
   approvalTier,
-}: HrApprovalStageProps) {}
+}: HrApprovalStageProps) {
+  if (status === "declined") {
+    // Hr
+    EmailSender({
+      to: approverEmail,
+      requestId: uuid,
+      message: "You have declined this travel requisition.",
+      title: "Final Update: Travel Requisition Declined",
+      role: "user",
+    });
+
+    // If submitter is the hod, only send one email
+    if (hodEmail === userEmail) {
+      EmailSender({
+        to: userEmail,
+        requestId: uuid,
+        message:
+          "Your travel requisition has been declined in the HR approval stage",
+        title: `Final Update: Travel Requisition Declined By ${approverName}`,
+        role: "user",
+      });
+    } else {
+      // HOD
+      EmailSender({
+        to: hodEmail,
+        requestId: uuid,
+        message:
+          "This travel requisition has been declined in the HR approval stage",
+        title: `Final Update: Travel Requisition Declined By ${approverName}`,
+        role: "user",
+      });
+      // Submitter
+      EmailSender({
+        to: userEmail,
+        requestId: uuid,
+        message:
+          "Your travel requisition has been declined in the HR approval stage",
+        title: `Final Update: Travel Requisition Declined By ${approverName}`,
+        role: "user",
+      });
+    }
+  }
+
+  if (status === "approved") {
+    if (approvalTier === "Tier 2") {
+      // Hr
+      EmailSender({
+        to: approverEmail,
+        requestId: uuid,
+        message: "You have approved this travel requisition",
+        title: "Final Update: Travel Requisition Approved",
+        role: "user",
+        showPdfDownload: true,
+      });
+
+      if (hodEmail === userEmail) {
+        EmailSender({
+          to: userEmail,
+          requestId: uuid,
+          message: `Your travel requisition has been approved by ${approverName}`,
+          title: `Final Update: Travel Requisition Approved By ${approverName}`,
+          role: "user",
+          showPdfDownload: true,
+        });
+      } else {
+        // Hod
+        EmailSender({
+          to: hodEmail,
+          requestId: uuid,
+          message: `This travel requisition has been approved by ${approverName}`,
+          title: `Final Update: Travel Requisition Approved By ${approverName}`,
+          role: "user",
+          showPdfDownload: true,
+        });
+        // User
+        EmailSender({
+          to: userEmail,
+          requestId: uuid,
+          message: `Your travel requisition has been approved by ${approverName}`,
+          title: `Final Update: Travel Requisition Approved By ${approverName}`,
+          role: "user",
+          showPdfDownload: true,
+        });
+      }
+    } else {
+      // Requires the next stage - Director approval
+      DIRECTOR_ARRAY.forEach((directorApprover) => {
+        EmailSender({
+          to: directorApprover.email,
+          requestId: uuid,
+          message:
+            "A new travel requisition has been submitted and requires your approval",
+          title: "Action Required: New Travel Requisition",
+          role: "Director",
+          reviewLink: `?token=${directorApprover.uuid}&stage=director`,
+        });
+      });
+
+      // --- Send mail to involved parties---
+      // Hr
+      EmailSender({
+        to: approverEmail,
+        requestId: uuid,
+        message:
+          "You have approved this travel requisition. It has been forwarded to Director Approval for the next approval stage",
+        title: "Update: Travel Requisition Approved",
+        role: "user",
+      });
+
+      if (hodEmail === userEmail) {
+        EmailSender({
+          to: userEmail,
+          requestId: uuid,
+          message: `Your travel requisition has been approved by ${approverName} and has been forwaded to Director Approval for the next approval stage`,
+          title: `Update: Travel Requisition Approved By ${approverName}`,
+          role: "user",
+        });
+      } else {
+        // Hod
+        EmailSender({
+          to: hodEmail,
+          requestId: uuid,
+          message: `This travel requisition has been approved by ${approverName} and has been forwaded to Director Approval for the next approval stage`,
+          title: `Update: Travel Requisition Approved By ${approverName}`,
+          role: "user",
+        });
+
+        // User
+        EmailSender({
+          to: userEmail,
+          requestId: uuid,
+          message: `Your travel requisition has been approved by ${approverName} and has been forwaded to Director Approval for the next approval stage`,
+          title: `Update: Travel Requisition Approved By ${approverName}`,
+          role: "user",
+        });
+      }
+    }
+  }
+}
