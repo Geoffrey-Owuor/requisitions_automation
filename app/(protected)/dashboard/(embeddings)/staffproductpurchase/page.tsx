f20d3708c0bfb1ad3b7c29cc0cbcf78f7565a4b2
Geoffrey-Owuor <geoffreyowuor22@gmail.com>
2026-06-16 12:55:19 +0300

    Disable token rotation to allow subsequent approval links to work, staffproductpurchase base url edit

--- app/(protected)/dashboard/(embeddings)/staffproductpurchase/page.tsx
+++ app/(protected)/dashboard/(embeddings)/staffproductpurchase/page.tsx
@@ -3,12 +3,27 @@ import PurchaseIframeWrapper from "@/components/Iframes/PurchaseIframeWrapper";
 import crypto from "crypto";
 import { redirect } from "next/navigation";
 import { getSession } from "@/lib/session";
+import { headers } from "next/headers";
 
 export const metadata: Metadata = {
   title: "Staff Product Purchase",
   description: "Staff Product Purchase Embedded web app",
 };
+
 const page = async () => {
+  // Get the passed in base url to construct the right embedded app url to go to
+  const headerList = await headers();
+
+  // 1. Inspect headers sent by Nginx, falling back to local headers if accessed directly
+  const proto = headerList.get("x-forwarded-proto") || "http";
+  const host =
+    headerList.get("x-forwarded-host") ||
+    headerList.get("host") ||
+    "127.0.0.1:4000";
+
+  // 2. Mathematically construct the exact base URL the user is currently using
+  const baseUrl = `${proto}://${host}`;
+
   // Retrieve the decoded jose JWT session data
   const session = await getSession();
 
@@ -31,7 +46,7 @@ const page = async () => {
     .digest("hex");
 
   // 3. Construct the secure URL pointing to Nginx port 4443
-  const ssoUrl = `${process.env.STAFFPURCHASE_LINK}/api/external/sso?email=${encodeURIComponent(userEmail)}&timestamp=${timestamp}&signature=${signature}`;
+  const ssoUrl = `${baseUrl}/api/external/sso?email=${encodeURIComponent(userEmail)}&timestamp=${timestamp}&signature=${signature}`;
 
   return <PurchaseIframeWrapper ssoUrl={ssoUrl} />;
 };
--- serverActions/UpdateITRequisitionStatus.ts
+++ serverActions/UpdateITRequisitionStatus.ts
@@ -90,13 +90,15 @@ export async function UpdateITRequisitionStatus(
 
     await client.query(baseUpdateQuery, baseParams);
 
+    // THIS IS COMMENTED OUT SINCE TOKEN ROTATION CAUSES INVALID LINK
+    // ISSUES FOR SUBSEQUENT EMAIL APPROVAL NOTIFICATIONS
     // Rotate the approver's uuid
-    await client.query(
-      `UPDATE ${payload.stage}_array 
-      SET ${payload.stage}_uuid = gen_random_uuid()
-      WHERE ${payload.stage}_email = $1`,
-      [payload.approverEmail],
-    );
+    // await client.query(
+    //   `UPDATE ${payload.stage}_array
+    //   SET ${payload.stage}_uuid = gen_random_uuid()
+    //   WHERE ${payload.stage}_email = $1`,
+    //   [payload.approverEmail],
+    // );
 
     await client.query("COMMIT");
 
